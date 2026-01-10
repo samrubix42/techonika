47b81056a1cac03b4447df60f101b948d5cb5482
Saumy Anand <samcool3203@gmail.com>
2026-01-10 11:25:21 +0530

    Add initial Blade templates for Digital Marketing and Web Designing sections - Created index.blade.php for Digital Marketing with basic structure - Created index.blade.php for Web Designing with basic structure

---
+++ app/Livewire/Public/WebDesigning/Index.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Public\WebDesigning;
+
+use Livewire\Component;
+
+class Index extends Component
+{
+    public function render()
+    {
+        return view('livewire.public.web-designing.index');
+    }
+}
